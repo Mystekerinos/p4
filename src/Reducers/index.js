eb7254b9e22bbd65312565ae367a73fe61ff1756
Jordy Saint-Albin <jordy.saintalbin@MacBook-Pro-1.local>
2023-03-30 22:40:49 +0200

    fist-commit

--- src/Reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { itemsHaveError, itemsAreLoading, getCity } from "./items";
-
-export default combineReducers({
-  itemsHaveError,
-  itemsAreLoading,
-  getCity,
-});
